247e3dee626bae80ed2e3a5575143690c0114ad2
Jack Watson <jpw2065@g.rit.edu>
2020-11-11 23:12:46 -0500

    Main Menu Assests

---
+++ SplatterconGame/Assets/Scripts/MenuButtonHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtonHandler : MonoBehaviour
+{
+
+    public void ShowSplatter(GameObject splatter)
+    {
+        splatter.SetActive(true);
+    }
+
+    public void HideSplatter(GameObject splatter)
+    {
+        splatter.SetActive(false);
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+
+    public void AboutButton()
+    {
+
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
