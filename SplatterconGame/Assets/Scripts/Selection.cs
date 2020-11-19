d7b97e8d40a58e667d52a7916ad9537887983cdc
Jack Watson <watson.jack.p@gmail.com>
2020-11-19 04:47:36 -0500

    UI Buttons for Gameplae

---
+++ SplatterconGame/Assets/Scripts/Selection.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+enum SelectionOptions { BOOTH, TRAP, SPELL }
+public class SelectionScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button _boothButton;
+    [SerializeField]
+    private Button _trapButton;
+    [SerializeField]
+    private Button _spellButton;
+    [SerializeField]
+    private Button _selectionButton;
+    [SerializeField]
+    private Button _playButton;
+
+    private SelectionOptions currentSelection;
+
+    void Awake()
+    {
+        _boothButton.onClick.AddListener(BoothButton);
+        _trapButton.onClick.AddListener(TrapButton);
+        _spellButton.onClick.AddListener(SpellButton);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateSelection(SelectionOptions selectionOptions)
+    {
+
+    }
+
+
+    void BoothButton()
+    {
+        UpdateSelection(SelectionOptions.BOOTH);
+    }
+
+    void SpellButton()
+    {
+        UpdateSelection(SelectionOptions.TRAP);
+    }
+
+    void TrapButton()
+    {
+        UpdateSelection(SelectionOptions.SPELL);
+    }
+
+    void SelectionButton()
+    {
+
+    }
+
+
+}
